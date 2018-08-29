558bea82ff5d2cc34a77deff61c53a43fc53ed83
Alexis Le Dinh <alexisledinh@gmail.com>
2018-08-29 13:58:53 +0200

    Debug charts for anomaly detection

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -3,8 +3,45 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Components from '../components';
 import Actions from '../actions';
+import NVD3Chart from 'react-nvd3';
+import * as d3 from 'd3';
+
 
 const TimerangeSelector = Components.Misc.TimerangeSelector;
+
+
+const context = {
+  formatXAxis: (d) => (d3.time.format('%x')(new Date(d))),
+  formatYAxis: (d) => ('$' + d3.format(',.2f')(d)),
+};
+
+const xAxis = {
+  tickFormat: {
+    name:'formatXAxis',
+    type:'function',
+  }
+};
+
+const yAxis = {
+  tickFormat: {
+    name:'formatYAxis',
+    type:'function',
+  }
+};
+
+/* istanbul ignore next */
+const formatX = (d) => {
+  const date = new Date(d[0]);
+  return date.getTime();
+};
+
+/* istanbul ignore next */
+const formatY = (d) => (d[1]);
+
+const margin = {
+  right: 10,
+  left: 70,
+};
 
 // EventsContainer Component
 class EventsContainer extends Component {
@@ -20,6 +57,50 @@ class EventsContainer extends Component {
       nextProps.getData(nextProps.dates.startDate, nextProps.dates.endDate);
   }
 
+  formatData(data) {
+    const res = [
+      {
+        key: "Cost",
+        values: [],
+      },
+      {
+        key: "Anomaly",
+        values: [],
+        color: '#ff0000'
+      },
+    ];
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      res[0].values.push([element.date, element.cost]);
+      res[1].values.push([element.date, element.abnormal ? element.cost : 0]);
+    }
+    return res;
+  }
+
+  getChartPanel(title, data) {
+    return(
+      <div className="white-box" key={title}>
+        <h4>{title}</h4>
+        <NVD3Chart
+          id="barChart"
+          type="multiBarChart"
+          datum={data}
+          context={context}
+          xAxis={xAxis}
+          yAxis={yAxis}
+          margin={margin}
+          rightAlignYAxis={false}
+          clipEdge={false}
+          showControls={true}
+          showLegend={true}
+          stacked={false}
+          x={formatX}
+          y={formatY}
+          height={300}
+        />
+      </div>
+    )
+  }
 
   render() {
     const timerange = (this.props.dates ?  (
@@ -30,9 +111,20 @@ class EventsContainer extends Component {
       />
     ) : null);
 
+    let charts = [];
+    if (this.props.values && this.props.values.status && this.props.values.values) {
+      for (var key in this.props.values.values) {
+        if (this.props.values.values.hasOwnProperty(key)) {
+            charts.push(this.getChartPanel(key, this.formatData(this.props.values.values[key])));
+        }
+      }
+    }
+
     return (
       <div>
         {timerange}
+        <hr />
+        {charts}
       </div>
     );
   }
@@ -41,6 +133,7 @@ class EventsContainer extends Component {
 
 EventsContainer.propTypes = {
   dates: PropTypes.object.isRequired,
+  values: PropTypes.object.isRequired,
   accounts: PropTypes.arrayOf(PropTypes.object),
   getData: PropTypes.func.isRequired,
   setDates: PropTypes.func.isRequired,
@@ -49,7 +142,8 @@ EventsContainer.propTypes = {
 /* istanbul ignore next */
 const mapStateToProps = ({aws, events}) => ({
   dates: events.dates,
-  accounts: aws.accounts.selection
+  accounts: aws.accounts.selection,
+  values: events.values,
 });
 
 /* istanbul ignore next */
